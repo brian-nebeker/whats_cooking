59fd681f5686968a460bc40ccd0e06e6b0f4c460
bnebe <bnebeker95@gmail.com>
2023-02-27 09:45:34 -0700

    updated

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from cuisine_classifier import CookingClassifier
 if __name__ == "__main__":
     ingredients = ['bacon', 'salt', 'flour', 'mushrooms', 'carrots', 'celery', 'garlic', 'tomato paste', 'beef broth', 'bay leaves', 'thyme', 'parsley', 'rosemary', 'potatoes', 'peas']
 
-    c = CookingClassifier(do_train=True, pred_ingredients=ingredients)
+    c = CookingClassifier(pred_ingredients=ingredients)
     c.execute()
